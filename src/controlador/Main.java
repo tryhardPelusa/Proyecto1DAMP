b96bfe5edceb9c6375af9f65b821b0abbfce8ec4
daniel <danimc2002.dmc@gmail.com>
2023-05-11 09:42:44 +0200

    nuevas ventanas en el main

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 import modelo.Modelo;
 import vista.Vista;
 import vista._00_PaginaPrincipal;
-import vista._01_InicioSesion;
+import vista._01_InicioSesion2;
 import vista._02_Registro;
 import vista._03_RecuperarCuenta;
 import vista._04_MiCuenta;
@@ -13,7 +13,7 @@ import vista._05_UnirseLiga;
 import vista._06_UnirseEquipo;
 import vista._07_CrearLigas;
 import vista._08_CrearEquipos;
-import vista._09_LigaEspecifica;
+import vista._09_LigaEspecifica2;
 import vista._10_EquipoEspecifico;
 import vista._11_Apuestas;
 import vista._12_MisApuestas;
@@ -28,7 +28,7 @@ public class Main {
 
 		Vista[] misVistas = new Vista[14];
 		misVistas[0] = new _00_PaginaPrincipal();
-		misVistas[1] = new _01_InicioSesion();
+		misVistas[1] = new _01_InicioSesion2();
 		misVistas[2] = new _02_Registro();
 		misVistas[3] = new _03_RecuperarCuenta();
 		misVistas[4] = new _04_MiCuenta();
@@ -36,7 +36,7 @@ public class Main {
 		misVistas[6] = new _06_UnirseEquipo();
 		misVistas[7] = new _07_CrearLigas();
 		misVistas[8] = new _08_CrearEquipos();
-		misVistas[9] = new _09_LigaEspecifica();
+		misVistas[9] = new _09_LigaEspecifica2();
 		misVistas[10] = new _10_EquipoEspecifico();
 		misVistas[11] = new _11_Apuestas();
 		misVistas[12] = new _12_MisApuestas();
