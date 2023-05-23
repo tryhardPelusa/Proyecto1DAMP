a0ffb076b2e3adeba9ec9bf5d122c794a04d4186
Wyllop <guillermo.callizaya@live.u-tad.com>
2023-05-23 18:49:31 +0200

    Conexion a la Base de Datos "ProyectoIntegrador"

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -1,5 +1,9 @@
 package controlador;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.swing.JFrame;
 
 import modelo.Modelo;
@@ -11,6 +15,12 @@ public class Controlador {
 	private Modelo miModelo;
 	private Vista[] misVistas;
 	private _02_Registro registro;
+	private String db = "ProyectoIntegrador";
+	private String login = "root";
+	private String pwd = "";
+	private String url = "jdbc:mysql://localhost/" + db
+			+ "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	private Connection conexion;
 
 	public void setVista(Vista[] misVistas) {
 		this.misVistas = misVistas;
@@ -33,7 +43,7 @@ public class Controlador {
 		String fecha = registro.getfecha();
 		String Pwd = registro.getPwd();
 		String User = registro.getUser();
-
+		ConexionMySQL();
 		registrarse(nombre, apellido1, apellido2, correo, fecha, Pwd, User);
 	}
 
@@ -41,4 +51,22 @@ public class Controlador {
 			String User) {
 
 	}
+
+	public void ConexionMySQL() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conexion = DriverManager.getConnection(url, login, pwd);
+			System.out.println("-> Conexión con MySQL establecida");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Driver JDBC No encontrado");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Error al conectarse a la BD");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Error general de Conexion");
+			e.printStackTrace();
+		}
+	}
+
 }
